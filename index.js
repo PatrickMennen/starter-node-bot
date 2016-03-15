2e2bfff4a1329db91a13b16de78725614f7b7c92
code1\310100427 <patrick.mennen@philips.com>
2016-03-15 08:37:53 +0100

    Removed wit

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-var Botkit = require('botkit');
-var wit = require('node-wit');
-var token = 'SRHS46GUPVLERG6MP65SYX33JHFKXRNM';
+var Botkit = require('botkit')
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN;
+var slackToken = process.env.SLACK_TOKEN
 if (!slackToken) {
-  console.error('SLACK_TOKEN is required!');
-  process.exit(1);
+  console.error('SLACK_TOKEN is required!')
+  process.exit(1)
 }
 
-var controller = Botkit.slackbot();
+var controller = Botkit.slackbot()
 var bot = controller.spawn({
   token: slackToken
-});
+})
 
 bot.startRTM(function (err, bot, payload) {
   if (err) {
-    throw new Error('Could not connect to Slack');
+    throw new Error('Could not connect to Slack')
   }
-});
+})
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "I'm here!")
+})
+
+controller.hears('.*', ['mention'], function (bot, message) {
+  bot.reply(message, 'You really do care about me. :heart:')
+})
+
+controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+  var help = 'I will respond to the following messages: \n' +
+      '`bot hi` for a simple message.\n' +
+      '`bot attachment` to see a Slack attachment message.\n' +
+      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+      '`bot help` to see this again.'
+  bot.reply(message, help)
+})
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  wit.captureTextIntent(token, message, function(err, result) {
-    if (err) {
-      bot.reply('I am sorry, the following error occured: `'  +  err + '`');
-    }
-    else {
-      bot.reply(JSON.stringify(res, null, ' '));
-    }
-  });
-});
+  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+})
